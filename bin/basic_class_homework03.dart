c70707de722c97b2ba7f7529915ef82bc568060e
Ozodbekyum <hello7ozod@gamil.com>
2022-09-19 15:06:59 +0500

    class03

--- bin/basic_class_homework03.dart
+++ bin/basic_class_homework03.dart
@@ -5,11 +5,10 @@ Create a "Person" class.
 Create an attribute "name" in the "Person" class.
 create an object named "person" whose name is "Ali"
 */
-
-void main() {
-  Person name = Person();
-}
-
 class Person {
   String name = 'Ali';
+}
+
+void main() {
+  Person person = Person();
 }
