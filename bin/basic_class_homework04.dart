fa5d9d2989645d9e1c9fed137c3e24fbe52640f5
Ozodbekyum <hello7ozod@gamil.com>
2022-09-19 15:27:20 +0500

    class04

--- bin/basic_class_homework04.dart
+++ bin/basic_class_homework04.dart
@@ -6,4 +6,12 @@ Create an attribute "name" in the "Person" class.
 create an object named "p1" whose name is "Anvar" 
 create an object named "p2" whose name is "Shavkat"
 */
-void main() {}
+class Person {
+  String name = 'komil';
+  Person(String l) {}
+}
+
+void main() {
+  Person p1 = Person('Anvar');
+  Person p2 = Person('Shavkat');
+}
