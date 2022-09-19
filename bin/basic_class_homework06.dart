e4d314f092488f4790572caf2dfe1f58066da1e5
Ozodbekyum <hello7ozod@gamil.com>
2022-09-19 15:48:18 +0500

    class06

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -3,5 +3,15 @@
 /*
 Create a "Person" class that has a name("name") and a age("age")
 */
+class Person {
+  String name = 'ali';
+  int age = 19;
+  Person(String n, int o) {
+    this.name = n;
+    this.age = o;
+  }
+}
 
-void main() {}
+void main() {
+  Person person = Person('Ozodbek', 16);
+}
